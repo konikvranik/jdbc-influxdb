79d027b5f5839107341b0b4eca3a41933e6e99c1
pvranik <petr.vranik@mgm-tp.com>
2022-09-27 23:00:07 +0200

    property info

--- src/main/java/net/suteren/jdbc/influxdb/InfluxDbDriver.java
+++ src/main/java/net/suteren/jdbc/influxdb/InfluxDbDriver.java
@@ -5,56 +5,87 @@ import java.net.URL;
 import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
+
+import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 
 @Log
 public class InfluxDbDriver implements java.sql.Driver {
+
+	public static final String USERNAME_PROPERTY = "username";
+	public static final String PASSWORD_PROPERTY = "password";
+	public static final String DATABASE_PROPERTY = "database";
+	public static final String USER_PROPERTY = "user";
+
 	@Override public InfluxDbConnection connect(String url, Properties info) throws SQLException {
 		Pattern p = Pattern.compile("jdbc:influxdb:(.*)");
 		Matcher m = p.matcher(url);
 		if (m.matches()) {
 			url = m.group(1);
-			String username = null;
-			String password = null;
-			AtomicReference<String> database = new AtomicReference<>();
-			if (info != null) {
-				username = info.getProperty("username");
-				if (username == null) {
-					username = info.getProperty("user");
-				}
-				password = info.getProperty("password");
-				database.set(info.getProperty("database"));
+			url = url.matches("^https?://.*$") ? url : "http://" + url;
+			if (info == null) {
+				info = parseUrlParams(url);
+			} else {
+				info.putAll(parseUrlParams(url));
 			}
-			try {
-				url = url.matches("^https?://.*$") ? url : "http://" + url;
-				Map<String, Set<String>> params = Arrays.stream(new URL(url).getQuery().split("&"))
-					.map(x -> x.split("=", 2))
-					.collect(Collectors.groupingBy(x -> x[0], Collectors.mapping(x -> x[1], Collectors.toSet())));
-				params.get("db").forEach(database::set);
-			} catch (MalformedURLException e) {
-				throw new SQLException(String.format("Invalid URL %s", url), e);
-			}
-			return new InfluxDbConnection(url, username, password, database.get(), this);
+			return new InfluxDbConnection(url, info.getProperty(USERNAME_PROPERTY, info.getProperty(USER_PROPERTY)),
+				info.getProperty(PASSWORD_PROPERTY), info.getProperty(DATABASE_PROPERTY), this);
 		} else {
 			throw new java.sql.SQLException(String.format("Invalid URL %s", url));
 		}
 	}
 
+	@SneakyThrows private static Properties parseUrlParams(String url) {
+		try {
+			URL url1 = new URL(url);
+			String[] ui = Optional.ofNullable(url1.getUserInfo()).map(u -> u.split(":", 2)).orElse(null);
+
+			Map<String, String> properties = Arrays.stream(url1.getQuery().split("&"))
+				.map(x -> x.split("=", 2))
+				.collect(Collectors.groupingBy(x -> x[0], Collectors.mapping(x -> x[1], Collectors.joining(","))));
+			if (ui != null && ui.length > 0 && StringUtils.isNotBlank(ui[0])) {
+				properties.put(USERNAME_PROPERTY, ui[0]);
+			}
+			if (ui != null && ui.length > 1 && StringUtils.isNotBlank(ui[1])) {
+				properties.put(PASSWORD_PROPERTY, ui[1]);
+			}
+			Properties properties1 = new Properties();
+			properties1.putAll(properties);
+			return properties1;
+		} catch (MalformedURLException e) {
+			throw new SQLException(String.format("Invalid URL %s", url), e);
+		}
+	}
+
 	@Override public boolean acceptsURL(String url) {
-		return url != null && url.startsWith("jdbc:inbluxdb:");
+		return url != null && url.startsWith("jdbc:influxdb:");
 	}
 
 	@Override public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) {
-		return new DriverPropertyInfo[0];
+		Set<DriverPropertyInfo> propertyInfos = new HashSet<>();
+		info.putAll(parseUrlParams(url));
+		if (StringUtils.isBlank(info.getProperty("db"))) {
+			propertyInfos.add(new DriverPropertyInfo(DATABASE_PROPERTY, null));
+		}
+		if (StringUtils.isBlank(info.getProperty(USERNAME_PROPERTY)) && StringUtils.isBlank(info.getProperty(
+			USER_PROPERTY))) {
+			propertyInfos.add(new DriverPropertyInfo(USERNAME_PROPERTY, null));
+		}
+		if (StringUtils.isBlank(info.getProperty(PASSWORD_PROPERTY))) {
+			propertyInfos.add(new DriverPropertyInfo(PASSWORD_PROPERTY, null));
+		}
+		return propertyInfos.toArray(DriverPropertyInfo[]::new);
 	}
 
 	@Override public int getMajorVersion() {
